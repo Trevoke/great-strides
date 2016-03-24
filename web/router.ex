9dc9e4a6832bc554615c294fe0429252c93105d9
Aldric Giacomoni <trevoke@gmail.com>
2016-03-23 23:47:32 -0400

    midnight oil

--- web/router.ex
+++ web/router.ex
@@ -14,23 +14,19 @@ defmodule GreatStrides.Router do
     plug :accepts, ["json"]
   end
 
-  pipeline :auth do
-    Ueberauth.plug "/auth"
-  end
-
   scope "/auth", GreatStrides do
-    pipe_through [:browser, :auth]
+    pipe_through [:browser]
 
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
     delete "/logout", AuthController, :delete
-    resources "/engagements", EngagementController
   end
 
   scope "/", GreatStrides do
     pipe_through [:browser] # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/engagements", EngagementController
   end
 
   # Other scopes may use custom stacks.
