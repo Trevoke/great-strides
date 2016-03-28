57f290eadf8bc1136f3057d60a45a4339047b3cf
Aldric Giacomoni <trevoke@gmail.com>
2016-03-27 21:34:14 -0400

    Merp?

--- web/controllers/mail_controller.ex
+++ web/controllers/mail_controller.ex
@@ -2,7 +2,7 @@ defmodule GreatStrides.MailController do
 	use GreatStrides.Web, :controller
   require Logger
 
-  def create(conn, %{"message" => message}) do
-    Logger.info message
+  def create(_conn, params) do
+    Logger.info params
   end
 end
