0b0c9096f0f9390c50c062f8b7f25d2471c61a7e
Aldric Giacomoni <trevoke@gmail.com>
2016-04-03 12:36:58 -0400

    Email date too

--- web/controllers/mail_controller.ex
+++ web/controllers/mail_controller.ex
@@ -1,12 +1,12 @@
 defmodule GreatStrides.MailController do
-	use GreatStrides.Web, :controller
+  use GreatStrides.Web, :controller
   require Logger
-  require IEx
 
   def create(conn, params) do
-    foo = "#{params["headers"]["Subject"]}"
-    bar = "#{params["reply_plain"]}"
-    Logger.info "\n#{foo}\n#{bar}"
+    foo = params["headers"]["Subject"]
+    bar = params["reply_plain"]
+    date = params["headers"]["Date"]
+    Logger.info "\n#{date}\n#{foo}\n#{bar}"
     render conn, :ok
   end
 end
