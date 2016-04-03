0b5411b57e934e65fc80a6b8cfcd567fc96b4cfb
Aldric Giacomoni <trevoke@gmail.com>
2016-04-03 17:37:16 -0400

    Merp

--- priv/repo/migrations/20160403204939_engagement_belongs_to_organization.exs
+++ priv/repo/migrations/20160403204939_engagement_belongs_to_organization.exs
@@ -2,7 +2,7 @@ defmodule GreatStrides.Repo.Migrations.EngagementBelongsToOrganization do
   use Ecto.Migration
 
   def change do
-    alter_table :organizations do
+    alter table :organizations do
       add :engagement_id, references(:engagement, on_delete: :nothing)
     end
   end
