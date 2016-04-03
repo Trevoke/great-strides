030bb6346cbce3f8f0e40782d27505d0f31a02f9
Aldric Giacomoni <trevoke@gmail.com>
2016-04-03 17:42:08 -0400

    Oh, Ecto

--- priv/repo/migrations/20160403210604_create_user.exs
+++ priv/repo/migrations/20160403210604_create_user.exs
@@ -2,7 +2,7 @@ defmodule GreatStrides.Repo.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
-    create_table :users do
+    create table :users do
       add :organization_id, references(:organizations, on_delete: :nothing)
       add :engagement_id, references(:engagements, on_delete: :nothing)
       add :username, :string
