4944b682660abfab78fddae47257310d909fea6a
Aldric Giacomoni <trevoke@gmail.com>
2016-04-03 12:39:49 -0400

    Log all the things

--- web/controllers/mail_controller.ex
+++ web/controllers/mail_controller.ex
@@ -3,10 +3,14 @@ defmodule GreatStrides.MailController do
   require Logger
 
   def create(conn, params) do
+    Logger.info "----------"
+    Logger.info params
+    Logger.info "----------"
     foo = params["headers"]["Subject"]
     bar = params["reply_plain"]
     date = params["headers"]["Date"]
     Logger.info "\n#{date}\n#{foo}\n#{bar}"
+    Logger.info "----------"
     render conn, :ok
   end
 end
