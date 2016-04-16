0dbb9748ed54d9310e12b4abf37aa7368227e8aa
Aldric Giacomoni <trevoke@gmail.com>
2016-04-15 22:12:05 -0400

    maybe I'm an idiot

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,18 +1,15 @@
 defmodule GreatStrides.PageController do
   use GreatStrides.Web, :controller
 
-  require Logger
-
-  def index(%{assigns: %{current_user: current_user}} = conn, _params) do
-    conn
-    |> assign(:current_user, current_user)
-    |> redirect(to: p_organization_path(conn, :show, current_user.organization_id))
-  end
-
   def index(conn, _params) do
-    Logger.info conn
-    conn
-    |> redirect(to: auth_path(conn, :index))
+    user = get_session(conn, :user)
+    if user do
+      conn
+      |> redirect(to: p_organization_path(conn, :show, user.organization_id))
+    else do
+      conn
+      |> redirect(to: auth_path(conn, :index))
+    end
   end
 
 end
