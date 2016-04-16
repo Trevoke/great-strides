c844a0ddd45993464e8c256d585e6c059abe26f2
Aldric Giacomoni <trevoke@gmail.com>
2016-04-15 21:46:00 -0400

    maybe I'm an idiot

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule GreatStrides.PageController do
   use GreatStrides.Web, :controller
 
+  require Logger
+
   def index(%{assigns: %{current_user: current_user}} = conn, _params) do
     conn
     |> assign(:current_user, current_user)
@@ -8,6 +10,7 @@ defmodule GreatStrides.PageController do
   end
 
   def index(conn, _params) do
+    Logger.info conn
     conn
     |> redirect(to: auth_path(conn, :index))
   end
