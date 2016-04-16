c1ef02ecc4bd7a7c7f5ad85c55d5a5d66584166f
Aldric Giacomoni <trevoke@gmail.com>
2016-04-16 13:16:16 -0400

    maybe I'm an idiot

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,8 @@
 defmodule GreatStrides.PageController do
   use GreatStrides.Web, :controller
 
-  require Logger
-
   def index(conn, _params) do
     user = get_session(conn, :user_id)
-    Logger.info "INSIDE / #{user}"
     if user do
       conn
       |> redirect(to: p_organization_path(conn, :show, 1))
