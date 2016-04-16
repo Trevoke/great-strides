8c64a07878a79aec0d3fd20fda15e1cc009ec25c
Aldric Giacomoni <trevoke@gmail.com>
2016-04-15 21:26:01 -0400

    maybe I'm an idiot

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,16 +1,14 @@
 defmodule GreatStrides.PageController do
   use GreatStrides.Web, :controller
 
-  def index(conn, _params) do
-    current_user = get_session(conn, :current_user)
-    if current_user do
-      conn
-      |> redirect(to: p_organization_path(conn, :show, current_user.organization_id))
-    else
-      conn
-      |> redirect(to: auth_path(conn, :index))
-    end
+  def index(%{assigns: %{current_user: current_user}}, _params) do
+    conn
+    |> redirect(to: p_organization_path(conn, :show, current_user.organization_id))
+  end
 
+  def index(conn, _params) do
+    conn
+    |> redirect(to: auth_path(conn, :index))
   end
 
 end
