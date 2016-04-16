b0ed87701c3e968ed0787af6f6d735eac3a702c4
Aldric Giacomoni <trevoke@gmail.com>
2016-04-15 21:15:25 -0400

    I did know how to use session

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,16 +1,16 @@
 defmodule GreatStrides.PageController do
   use GreatStrides.Web, :controller
 
-
-  def index(%{assigns: %{current_user: current_user}} = conn, _params) do
-    conn
-    |> redirect(to: p_organization_path(conn, :show, current_user.organization_id))
-  end
-
-
   def index(conn, _params) do
-    conn
-    |> redirect(to: auth_path(conn, :index))
+    current_user = get_session(conn, :current_user)
+    if current_user do
+      conn
+      |> redirect(to: p_organization_path(conn, :show, current_user.organization_id))
+    else
+      conn
+      |> redirect(to: auth_path(conn, :index))
+    end
+
   end
 
 end
